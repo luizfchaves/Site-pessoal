a16c3f99041ac2766193d213c342cc3a7600021b
Luiz <lf1405@hotmail.com>
2020-02-29 11:12:11 -0300

    starting with the navbar

---
+++ src/colors.js
@@ -0,0 +1,12 @@
+exports.color = {
+  dark: {
+    "--background-color": "#222222",
+    "--text-color": "#ffffff",
+    "--primary-color": "#64B6AC"
+  },
+  light: {
+    "--background-color": "#EAEAEA",
+    "--text-color": "#474747",
+    "--primary-color": "#64B6AC"
+  }
+};
