e81b38e6f86d8bf6e5a72a571e8f1cb52a69b306
Luiz <lf1405@hotmail.com>
2020-03-05 00:59:59 -0300

    Removendo letras azuis do nome

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -7,11 +7,7 @@ const AboutMe = () => {
     <>
       <div id="about-me" className="section">
         <p className="title text-center">Sobre mim</p>
-        <p className="sub-title">
-          <span style={{ color: "var(--primary-color)" }}>L</span>uiz Fernando
-          Alves de Souza{" "}
-          <span style={{ color: "var(--primary-color)" }}>C</span>haves
-        </p>
+        <p className="sub-title">Luiz Fernando Alves de Souza Chaves</p>
         <p className="text">Desenvolvedor Junior</p>
         <p className="text distance-above">isso</p>
       </div>
